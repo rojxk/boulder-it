cdde981b4af2cb31193c67a600cfb995222a912d
Magda <magda.rojek2@gmail.com>
2024-10-22 21:14:39 +0200

    Initial commit

---
+++ src/main/java/com/boulderit/BoulderitApplication.java
@@ -0,0 +1,13 @@
+package com.boulderit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoulderitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoulderitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/boulderit/BoulderitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boulderit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoulderitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
