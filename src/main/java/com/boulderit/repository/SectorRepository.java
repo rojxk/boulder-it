838a11f47b4a8ca439828afa9fe70e741895163f
Magda <magda.rojek2@gmail.com>
2025-01-03 15:42:43 +0100

    Update SectorRepository

--- src/main/java/com/boulderit/repository/SectorRepository.java
+++ src/main/java/com/boulderit/repository/SectorRepository.java
@@ -2,9 +2,15 @@ package com.boulderit.repository;
 
 import com.boulderit.model.Sector;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface SectorRepository extends MongoRepository<Sector, String> {
+
+    @Query("{ 'area_id': ?0 }")
+    List<Sector> findByAreaId(String areaId);
 
 }
