d6b4c37bf39fbb14fa0d9c4d4dd5d416b52396fc
Magda <magda.rojek2@gmail.com>
2025-01-03 15:40:53 +0100

    Update Parking Repo

--- src/main/java/com/boulderit/repository/ParkingSpotRepository.java
+++ src/main/java/com/boulderit/repository/ParkingSpotRepository.java
@@ -3,11 +3,15 @@ package com.boulderit.repository;
 
 import com.boulderit.model.ParkingSpot;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 
 
 @Repository
 public interface ParkingSpotRepository extends MongoRepository<ParkingSpot, String> {
+
+    @Query("{ '_id': ?0 }")
+    ParkingSpot findByAreaId(String areaId);
 
 }
