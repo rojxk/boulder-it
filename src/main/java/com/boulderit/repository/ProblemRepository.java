fe0cba4a9255b4c1225785db3d83a330f9c62e7c
Magda <magda.rojek2@gmail.com>
2025-01-03 15:41:08 +0100

    Update ProblemRepository

--- src/main/java/com/boulderit/repository/ProblemRepository.java
+++ src/main/java/com/boulderit/repository/ProblemRepository.java
@@ -2,8 +2,16 @@ package com.boulderit.repository;
 
 import com.boulderit.model.Problem;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ProblemRepository extends MongoRepository<Problem, String> {
+
+    @Query("{ 'sector_id': ?0 }")
+    List<Problem> findBySectorId(String sectorId);
+
+
 }
