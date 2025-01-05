519da44c7218e6580fa701d13b7a0794d56b735e
Magda <magda.rojek2@gmail.com>
2025-01-05 22:23:42 +0100

    Update ParkingController

--- src/main/java/com/boulderit/controller/ParkingController.java
+++ src/main/java/com/boulderit/controller/ParkingController.java
@@ -1,22 +1,27 @@
 package com.boulderit.controller;
 
 import com.boulderit.model.ParkingSpot;
+import com.boulderit.service.AreaService;
 import com.boulderit.service.ParkingService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.List;
 
 @Controller
 @RequestMapping("/api/parking")
 public class ParkingController {
     private ParkingService parkingService;
+    private AreaService areaService;
 
     @Autowired
-    public ParkingController(ParkingService parkingService) {
+    public ParkingController(ParkingService parkingService, AreaService areaService) {
         this.parkingService = parkingService;
+        this.areaService = areaService;
     }
 
     @GetMapping
@@ -30,13 +35,23 @@ public class ParkingController {
     }
 
     @PostMapping
-    public  ResponseEntity<ParkingSpot> createParkingSpot(@RequestBody ParkingSpot parkingSpot) {
-        return ResponseEntity.ok(parkingService.save(parkingSpot));
+    public ResponseEntity<ParkingSpot> createParkingSpot(@RequestBody ParkingSpot parkingSpot, @RequestParam String areaId) {
+        parkingSpot.setCreatedAt(new Date());
+        parkingSpot.setUpdatedAt(new Date());
+        ParkingSpot saved = parkingService.save(parkingSpot);
+        areaService.addParkingSpot(areaId, saved);
+        return ResponseEntity.status(HttpStatus.CREATED).body(saved);
     }
 
     @PutMapping("/{id}")
     public  ResponseEntity<ParkingSpot> updateParkingSpot(@PathVariable String id, @RequestBody ParkingSpot parkingSpot) {
         return ResponseEntity.ok(parkingService.update(parkingSpot, id));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteParkingSpot(@PathVariable String id, @RequestParam String areaId) {
+        parkingService.delete(id);
+        return ResponseEntity.noContent().build();
     }
 
 
