c92d6cbcda6ed7b796d06249adaabbc5afcf89c0
Magda <magda.rojek2@gmail.com>
2025-01-06 14:08:11 +0100

    Update CommentRepository

--- src/main/java/com/boulderit/repository/CommentRepository.java
+++ src/main/java/com/boulderit/repository/CommentRepository.java
@@ -1,10 +1,17 @@
 package com.boulderit.repository;
 
 import com.boulderit.model.Comment;
+import com.boulderit.model.Problem;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface CommentRepository extends MongoRepository<Comment, String> {
+
+    @Query("{ 'problem_id': ?0 }")
+    List<Comment> findByProblemId(String problemId);
 
 }
