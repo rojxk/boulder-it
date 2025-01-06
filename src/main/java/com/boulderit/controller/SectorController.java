e71dd89c212c02c25cbaf1e3dbc0c2d02bc211d1
Magda <magda.rojek2@gmail.com>
2025-01-06 16:22:07 +0100

    Update SectorController

--- src/main/java/com/boulderit/controller/SectorController.java
+++ src/main/java/com/boulderit/controller/SectorController.java
@@ -1,17 +1,18 @@
 package com.boulderit.controller;
 
+import com.boulderit.model.Comment;
 import com.boulderit.model.Problem;
 import com.boulderit.model.Sector;
+import com.boulderit.service.CommentService;
 import com.boulderit.service.ProblemService;
 import com.boulderit.service.SectorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/sectors")
@@ -19,11 +20,13 @@ public class SectorController {
 
     private ProblemService problemService;
     private SectorService sectorService;
+    private CommentService commentService;
 
     @Autowired
-    public SectorController(ProblemService problemService, SectorService sectorService){
+    public SectorController(ProblemService problemService, SectorService sectorService, CommentService commentService){
         this.problemService = problemService;
         this.sectorService = sectorService;
+        this.commentService = commentService;
     }
 
     @GetMapping("/{sectorId}")
@@ -32,7 +35,21 @@ public class SectorController {
     }
 
     @GetMapping("/{sectorId}/problems")
-    public ResponseEntity<List<Problem>> getProblemsBySector(@PathVariable String sectorId) {
-        return ResponseEntity.ok(problemService.findBySectorId(sectorId));
+    public ResponseEntity<Map<String, Object>> getProblemsBySector(@PathVariable String sectorId) {
+        List<Problem> problems = problemService.findBySectorId(sectorId);
+        Map<String, List<Comment>> commentsByProblem = new HashMap<>();
+
+        // Get comments for each problem
+        for (Problem problem : problems) {
+            List<Comment> comments = commentService.findByProblemId(problem.getId());
+            commentsByProblem.put(problem.getId(), comments);
+        }
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("problems", problems);
+        response.put("comments", commentsByProblem);
+
+        return ResponseEntity.ok(response);
     }
+
 }
